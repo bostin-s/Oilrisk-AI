741093ee6e6175ce812635984d2b80847ac42e94
S.Bostin <bostinbos@gmail.com>
2026-03-23 14:58:48 +0530

    App.py hosting commit — OilRisk AIGlobal Edition

--- Oilrisk_global/app.py
+++ Oilrisk_global/app.py
@@ -413,4 +413,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
+    _port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=_port, threaded=True)
